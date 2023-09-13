843dbd4970215a3556d1ffa3b674e996b48c621e
Amar Gawade <amarspggawade@gmail.com>
2023-09-13 00:55:35 -0400

    Added badge and profile functionality

--- iOSDev/SwiftUI/SwiftUITutorialApple/SwiftUITutorialApple/Views/Categories/CategoryHome.swift
+++ iOSDev/SwiftUI/SwiftUITutorialApple/SwiftUITutorialApple/Views/Categories/CategoryHome.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
+    @State private var showingProfile = false
 
     var body: some View {
         NavigationView {
@@ -21,17 +22,27 @@ struct CategoryHome: View {
                     .clipped()
                     .listRowInsets(EdgeInsets())
 
-
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
-
 
 struct CategoryHome_Previews: PreviewProvider {
     static var previews: some View {
